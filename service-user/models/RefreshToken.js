ecf6a21fd1d43b8aa8741fa7a90384e0bb49e1e3
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-03-28 14:36:52 +0700

    add model user and refreshToken

---
+++ service-user/models/RefreshToken.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+  const RefreshToken = sequelize.define(
+    "RefreshToken",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      token: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: "created_at",
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: "updated_at",
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "refresh_tokens",
+      timestamps: true,
+    }
+  );
+  return RefreshToken;
+};
---
+++ service-user/models/User.js
@@ -0,0 +1,69 @@
+const bcrypt = require("bcrypt");
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "field nama harus di-isi",
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notNull: {
+            msg: "field email harus di-isi",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "field password harus di-isi",
+          },
+        },
+        set(value) {
+          this.setDataValue("password", bcrypt.hashSync(value, 10));
+        },
+      },
+      role: {
+        type: DataTypes.ENUM,
+        values: ["owner", "karyawan"],
+        defaultValue: "karyawan",
+        allowNull: false,
+      },
+      createdAt: {
+        //field aliases
+        field: "created_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        //field aliases
+        field: "updated_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "users",
+      timestamps: true,
+    }
+  );
+  return User;
+};
