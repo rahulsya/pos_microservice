8a40186d27bbf02ffc494792e6e2a029daeac26e
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-03-28 13:24:48 +0700

    intial commit

---
+++ service-user/migrations/20220328060313-create_users.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        values: ["owner", "karyawan"],
+        type: Sequelize.ENUM,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
