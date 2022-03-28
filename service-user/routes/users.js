0d0627420693815654e390a0bf11937c106c5442
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-03-28 14:38:28 +0700

    add route create user

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+// controller
+const { UserController } = require("../controller");
+const multer = require("multer");
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
+router.post("/create", multer().none(), UserController.Register);
 
 module.exports = router;
