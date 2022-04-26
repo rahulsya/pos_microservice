5fd7d44a347c58e1c0160ad965a519611728b356
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-04-26 16:02:39 +0700

    change method refresh token

--- api-gateway/app/refresh_token/router.js
+++ api-gateway/app/refresh_token/router.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const multer = require("multer");
 const RefreshToken = require("./handler/refresh_token");
 
-router.get("/", multer().none(), RefreshToken);
+router.post("/", multer().none(), RefreshToken);
 
 module.exports = router;
