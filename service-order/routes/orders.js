0ed2ae9a2a54ec27d292c2664f80969d8655098c
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-05-10 22:29:17 +0700

    add endpoint detail order

--- service-order/routes/orders.js
+++ service-order/routes/orders.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const multer = require("multer");
-const { index, store } = require("../controller/OrderController");
+const { index, store, order } = require("../controller/OrderController");
 
 router.get("/", index);
+router.get("/:id", order);
 router.post("/", multer().none(), store);
 
 module.exports = router;
